5fb2bbdc07e91795cd10e1a0361cdf91e9672970
GustavoII2 <gustavo.ayala@ufpi.edu.br>
2023-08-03 12:57:19 -0300

    uma pequena alteração

--- src/App.java
+++ src/App.java
@@ -1,7 +1,8 @@
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("\n1+1 = 625!\n");
+        //gugugugu
 
     }
 }
