bd7b0bbeb84e2955a0beda2ddc1ed840d77f8347
baba <vava@gamil.com>
2023-08-03 12:44:54 -0300

    commit inicial

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("\n1+1 = 625!\n");
+
+    }
+}
